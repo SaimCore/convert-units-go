b82a193e963ace95458fb6f13d1ed4d6adb2678f
PurinPintakhiew <dev.purin@gmail.com>
2025-01-04 23:33:53 +0700

    feat: convert weight test

--- convert/weight/weight_test.go
+++ convert/weight/weight_test.go
@@ -3,5 +3,13 @@ package weight
 import "testing"
 
 func TestConvertWeight(t *testing.T) {
-	
+	result, err := ConvertWeight(1, "kilogram", "gram")
+	if err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+
+	expected := 1000.000
+	if result != expected {
+		t.Errorf("result = %.3f; want %.3f\n", result, expected)
+	}
 }
